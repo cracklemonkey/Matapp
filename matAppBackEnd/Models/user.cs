12203cc86e7f2528ffd17fa61dccba14a08e4b31
daniel stupar <danielstupar95@gmail.com>
2021-08-23 14:38:52 +0200

    changed models

--- matAppBackEnd/Models/user.cs
+++ matAppBackEnd/Models/user.cs
@@ -8,32 +8,31 @@ namespace matAppBackEnd.Models
 {   
     public class User
     {
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
+
     public string Id { get; set; }
 
-    [BsonElement("username")]     
+     
     public string Username {get; set;}
 
-    [BsonElement("password")]     
+     
     public string Password {get; set;}
 
-    [BsonElement("email")]     
+      
     public string EMail {get; set;}
 
-    [BsonElement("phonenumber")]     
+     
     public string PhoneNumber {get; set;}
 
-    [BsonElement("street")]     
+       
     public string Street {get; set;}
 
-    [BsonElement("city")]     
+      
     public string City {get; set;}
 
-    [BsonElement("zipcode")]     
+      
     public string ZipCode {get; set;}
 
-    [BsonElement("country")]     
+     
     public string Country {get; set;}
 
 
